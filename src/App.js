fd0bbae69092f42fb352f79348985e423bd5bca1
axcell-0 <121089024+axcell-0@users.noreply.github.com>
2023-09-02 15:24:44 +0100

    Modify App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import BooksContainer from './pages/BooksContainer';
 import Navbar from './components/Navbar';
+import Categories from './pages/Categories';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <div className="container">
           <Routes>
             <Route path="/" element={<BooksContainer />} />
+            <Route path="/categories" element={<Categories />} />
           </Routes>
         </div>
       </div>
